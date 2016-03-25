9b9ac777952bf926e12a766658154ebcfeac4e12
Tim Kopplow <tim@arctium.org>
2016-03-25 21:31:04 +0100

    Init
    This right now is the „gulpified“ version of Miodec’s source. In this
    state everything, except for a few sass variables and some minor jquery
    stuff, is just the original code. Its build right now upon a
    Boilerplate called fastshell by Hossein Karami. Feel free to modify,
    add informations or even fork and do awesome stuff with this
    repository.

---
+++ app/assets/js/perks.js
@@ -0,0 +1,382 @@
+/*!
+ * Skill-calculator
+ * Skill Calculator for Tom Clancy's The Division
+ * http://miodec.com/division/
+ * @author Jacek Bartnik
+ * @version 0.1.0
+ * Copyright 2016. MIT licensed.
+ */
+var perData = {
+	med:{
+		per1:{
+			name:"Medkits",
+			desc:"Increase Medkit inventory by 1 slot",
+			req:"Clinic"
+		},
+		per2:{
+			name:"Protective Measures",
+			desc:"Increase Virus Protection by 1, gaining access to higher contamination areas.",
+			req:"Virus Lab"
+		},
+		per3:{
+			name:"Canned Food",
+			desc:"Increase Canned Food effect duration by 10 seconds",
+			req:"Disaster Aid"
+		},
+		per4:{
+			name:"Supplier",
+			desc:"Collect canned food, water, energy bar and soda in the Suppy Line once per hour.",
+			req:"Disaster Aid"
+		},
+		per5:{
+			name:"Contamination Intel",
+			desc:"Reveal all contaminated areas on the map",
+			req:"Quarantine"
+		},
+		per6:{
+			name:"Medkits",
+			desc:"Increase Medkit inventory by 1 slot",
+			req:"Intensive Care"
+		},
+		per7:{
+			name:"Protective Measures",
+			desc:"Increase Virus Protection by 1, gaining access to higher contamination areas.",
+			req:"Hazmat Unit"
+		},
+		per8:{
+			name:"Hazardous Materials",
+			desc:"Increase Dark Zone inventory by 1 slot.",
+			req:"Decontamination Unit"
+		},
+		per9:{
+			name:"Rigger",
+			desc:"Collect fabrics (crafting materials) in the Medical Wing once per hour.",
+			req:"Pharmacy"
+		},
+		per10:{
+			name:"Protective Measures",
+			desc:"Increase Virus Protection by 1, gaining access to higher contamination areas.",
+			req:"Pharmacy"
+		},
+		per11:{
+			name:"Credit",
+			desc:"Increase credit gains by 10%",
+			req:"Counseling"
+		},
+		per12:{
+			name:"Medkits",
+			desc:"Increase Medkit inventory by 1 slot",
+			req:"Pediatric Care"
+		},
+		per13:{
+			name:"Talents",
+			desc:"Increase active talents by 1 slot",
+			req:"Build all upgrades"
+		},
+	},
+	tech:{
+		per1:{
+			name:"Inventory",
+			desc:"Increase inventory by 10 slots",
+			req:"Control Room"
+		},
+		per2:{
+			name:"Hazardous Materials",
+			desc:"Increase Dark Zone inventory by 1 slot",
+			req:"Communications"
+		},
+		per3:{
+			name:"Division Tech Materials",
+			desc:"Convert Division Tech crafting materials into other materials at the crafting station",
+			req:"Division Tech"
+		},
+		per4:{
+			name:"Engineer",
+			desc:"Collect electronics (crafting materials) in the Tech Wing once per hour",
+			req:"4"
+		},
+		per5:{
+			name:"Soda",
+			desc:"Increase Soda consumable effect duration by 10 seconds",
+			req:"Generators"
+		},
+		per6:{
+			name:"Water",
+			desc:"Increase Water consumable effect duration by 10 seconds",
+			req:"Water Purification"
+		},
+		per7:{
+			name:"Craftsman",
+			desc:"Collect tools (crafting materials) in the Tech Wing once per hour",
+			req:"Field Engineering"
+		},
+		per8:{
+			name:"Energy Bar",
+			desc:"Increase enerby bar effect duration by 5 seconds",
+			req:"Central Heating"
+		},
+		per9:{
+			name:"Rescue Assessment",
+			desc:"Reveal all Division Tech crafting nodes on the Dark Zone map",
+			req:"SatComms"
+		},
+		per10:{
+			name:"Stat Switch",
+			desc:"Unlock item stat switching at the Recalibration Station",
+			req:"Recalibration"
+		},
+		per11:{
+			name:"Dark Zone Funds",
+			desc:"Increase looted Dark Zone fund gains by 10%",
+			req:"Recharge Center"
+		},
+		per12:{
+			name:"Stash",
+			desc:"Increase stash by 10 slots",
+			req:"Build all upgrades"
+		},
+	},
+	sec:{
+		per1:{
+			name:"Experienced Agent",
+			desc:"Increase XP gained from all activities by 10%",
+			req:"Situation Room"
+		},
+		per2:{
+			name:"Grenades",
+			desc:"Increase the grenade inventory by 1 slot",
+			req:"Armory"
+		},
+		per3:{
+			name:"Advanced Weaponry",
+			desc:"Unlock advanced weapons vendorin the Security Wing",
+			req:"Armory"
+		},
+		per4:{
+			name:"Combat Veteran",
+			desc:"Increase XP gained from accolades by 25%",
+			req:"Barracks"
+		},
+		per5:{
+			name:"Hazardous Materials",
+			desc:"Increase Dark Zone inventory by 1 slot",
+			req:"Dark Zone Ops"
+		},
+		per6:{
+			name:"Black Market Vendor",
+			desc:"Unlock Dark Zone fund purchases in the Base of Operations",
+			req:"Dark Zone Ops"
+		},
+		per7:{
+			name:"Clothes Vendor",
+			desc:"Unlock a clother venodr in the Base of Operations",
+			req:"Suppy Line"
+		},
+		per8:{
+			name:"Explosive Bullets",
+			desc:"Increase Explosive Bullets effect duration by 10 seconds",
+			req:"Gunsmith"
+		},
+		per9:{
+			name:"Gunsmith",
+			desc:"Collect weapon parts (crafting materials) in the Security Wing once per hour",
+			req:""
+		},
+		per10:{
+			name:"Scavenger",
+			desc:"Collect items from the scavenger box in the Security Wing once per hour",
+			req:"Gunsmith"
+		},
+		per11:{
+			name:"Incendiary Bullets",
+			desc:"Increase Incendiary Bullets effect duration by 10 seconds",
+			req:"Guard Posts"
+		},
+		per12:{
+			name:"Special Ammo",
+			desc:"Collect incendiary and explosive bullets (ammo) in the Supply Line ince per hour",
+			req:"Guard Posts"
+		},
+		per13:{
+			name:"Shooting Range",
+			desc:"Train in Marksmanship at the Shooting Range",
+			req:"Shooting Range"
+		},
+		per14:{
+			name:"Intel Discovery",
+			desc:"When completing all Side Missions and Encounters in a Named Zone, reveal all undiscovered Intel on the map",
+			req:"Canine Unit"
+		},
+		per15:{
+			name:"Collector",
+			desc:"Increase Scavenging by 15%",
+			req:"Build all upgrades"
+		},
+	}
+};
+
+var activePerk = "per1";
+var activeTree = "med";
+
+var choicePerk = "perMed1";
+$(".perkBox").click(function() {
+
+    $(this).addClass("boxToggle");
+
+    if (choicePerk === $(this).attr('id')) {
+
+    } else if (choicePerk !== "") {
+        $("#" + choicePerk).removeClass("boxToggle");
+        choicePerk = "";
+        choicePerk = $(this).attr('id');
+    }
+
+});
+
+function updatePerkIcons (tree) {
+	var y;
+	var tr;
+	var x = 75;
+
+	if (tree === "med") {
+    y = 0;
+    tr = "Med";
+  } else if (tree === "tech") {
+    y = 75;
+    tr = "Tech";
+  } else if (tree === "sec") {
+    y = 75*2;
+    tr = "Sec";
+  }
+
+  $("#per"+tr+1).css("background-position", "0px -"+y+"px");
+  $("#per"+tr+2).css("background-position", "-"+x+"px -"+y+"px");
+  $("#per"+tr+3).css("background-position", "-"+x*2+"px -"+y+"px");
+  $("#per"+tr+4).css("background-position", "-"+x*3+"px -"+y+"px");
+  $("#per"+tr+5).css("background-position", "-"+x*4+"px -"+y+"px");
+  $("#per"+tr+6).css("background-position", "-"+x*5+"px -"+y+"px");
+  $("#per"+tr+7).css("background-position", "-"+x*6+"px -"+y+"px");
+  $("#per"+tr+8).css("background-position", "-"+x*7+"px -"+y+"px");
+  $("#per"+tr+9).css("background-position", "-"+x*8+"px -"+y+"px");
+  $("#per"+tr+10).css("background-position", "-"+x*9+"px -"+y+"px");
+  $("#per"+tr+11).css("background-position", "-"+x*10+"px -"+y+"px");
+  $("#per"+tr+12).css("background-position", "-"+x*11+"px -"+y+"px");
+  $("#per"+tr+13).css("background-position", "-"+x*12+"px -"+y+"px");
+  $("#per"+tr+14).css("background-position", "-"+x*13+"px -"+y+"px");
+  $("#per"+tr+15).css("background-position", "-"+x*14+"px -"+y+"px");
+}
+
+updatePerkIcons('med');
+updatePerkIcons('tech');
+updatePerkIcons('sec');
+
+function updatePerkInfoIcon(tree) {
+	var y;
+	var tr;
+	var x;
+
+	if (tree === "med") {
+    y = 0;
+  } else if (tree === "tech") {
+    y = 100;
+  } else if (tree === "sec") {
+    y = 200;
+  }
+
+  if(activePerk.length == 4){
+  	x = parseFloat(activePerk.slice(-1)-1) * 100;
+  }else{
+  	x = parseFloat(activePerk.slice(-2)-1) * 100;
+  }
+
+  $(".perInfoPerkIcon").css("background-position", "-"+x+"px -"+y+"px");
+}
+
+function perkCheck(tree) {
+    // Change talent name
+    $("#perInfoPerkName").text(perData[tree][activePerk].name);
+    // Change talent description
+    $(".perInfoDesc").text(perData[tree][activePerk].desc);
+    if (perData[tree][activePerk].req === "") {
+        // Hide requirement if 'req' is empty
+        $('.perReq').css("display", "none");
+    } else if (perData[tree][activePerk].req !== "") {
+        // Show requirement if present
+        $('.perReq').css("display", "block");
+        // Change req text
+        $(".reqDetail").text(perData[tree][activePerk].req);
+        // Change icon depending on tree
+        if (activeTree === "med") {
+            $('.reqTree').text("Medical Tree Upgrade");
+            $('.reqIcon').css("background-position", "0 0");
+        } else if (activeTree === "tech") {
+            $('.reqTree').text("Tech Tree Upgrade");
+            $('.reqIcon').css("background-position", "-50px 0");
+        } else {
+            $('.reqTree').text("Security Tree Upgrade");
+            $('.reqIcon').css("background-position", "-100px 0");
+        }
+    }
+    if (activeTree === "med") {
+        $('.perInfoBox').css("border-top", "2px #6ec449 solid");
+        $('.perInfoBox').css("border-bottom", "2px #6ec449 solid");
+    } else if (activeTree === "tech") {
+        $('.perInfoBox').css("border-top", "2px #ffca57 solid");
+        $('.perInfoBox').css("border-bottom", "2px #ffca57 solid");
+    } else if (activeTree === "sec") {
+        $('.perInfoBox').css("border-top", "2px #4084c5 solid");
+        $('.perInfoBox').css("border-bottom", "2px #4084c5 solid");
+    }
+}
+
+function perkClick(tree,number){
+	$("#per"+tree+number).mousedown(function() {
+    activePerk = 'per'+number;
+    activeTree = tree.toLowerCase();
+    perkCheck(activeTree);
+    updatePerkInfoIcon(activeTree);
+	});
+}
+
+perkClick('Med',1);
+perkClick('Med',2);
+perkClick('Med',3);
+perkClick('Med',4);
+perkClick('Med',5);
+perkClick('Med',6);
+perkClick('Med',7);
+perkClick('Med',8);
+perkClick('Med',9);
+perkClick('Med',10);
+perkClick('Med',11);
+perkClick('Med',12);
+perkClick('Med',13);
+
+perkClick('Tech',1);
+perkClick('Tech',2);
+perkClick('Tech',3);
+perkClick('Tech',4);
+perkClick('Tech',5);
+perkClick('Tech',6);
+perkClick('Tech',7);
+perkClick('Tech',8);
+perkClick('Tech',9);
+perkClick('Tech',10);
+perkClick('Tech',11);
+perkClick('Tech',12);
+
+perkClick('Sec',1);
+perkClick('Sec',2);
+perkClick('Sec',3);
+perkClick('Sec',4);
+perkClick('Sec',5);
+perkClick('Sec',6);
+perkClick('Sec',7);
+perkClick('Sec',8);
+perkClick('Sec',9);
+perkClick('Sec',10);
+perkClick('Sec',11);
+perkClick('Sec',12);
+perkClick('Sec',13);
+perkClick('Sec',14);
+perkClick('Sec',15);
---
+++ src/js/talents.js
@@ -0,0 +1,303 @@
+var talData = {
+    med: {
+        tal1: {
+            name: "Adrenaline",
+            desc: "Use a medkit when not at full health to receive an overheal",
+            req: "Decontamination Unit",
+            benefits: ["Overheal", "Affects Agent"],
+        },
+        tal2: {
+            name: "Triage",
+            desc: "Heal an ally with a skill to reduce skill cooldowns by 15%",
+            req: "Decontamination Unit",
+            benefits: ["Cooldown Reduction", "Affects Group"],
+        },
+        tal3: {
+            name: "Shock and Awe",
+            desc: "Suppress a hostile to increase movement speed by 25% for 10 seconds",
+            req: "Pharmacy",
+            benefits: ["Movement Speed", "Affects Agent"],
+        },
+        tal4: {
+            name: "Battle Buddy",
+            desc: "Revive a downed agent to reduce incoming damage by 50% for 10 seconds for both players",
+            req: "Pharmacy",
+            benefits: ["Damage Reduction", "Affects Group"],
+        },
+        tal5: {
+            name: "Critical Save",
+            desc: "Use a medkit during low health to increase damage resistance by 40% for 10 seconds",
+            req: "Counselling",
+            benefits: ["Damage Resistance", "Affects Agent"],
+        },
+        tal6: {
+            name: "Shrapnel",
+            desc: "Applying Bleed to any target triggers a 30% chance to apply the effect to all targets within 10m",
+            req: "Counselling",
+            benefits: ["Bleed Chance", "Affects Hostiles"],
+        },
+        tal7: {
+            name: "Combat Medic",
+            desc: "Use a medkit near allies to heal group members and proxies within 20m by 40%",
+            req: "",
+            benefits: ["Healing", "Affects Group"],
+        },
+        tal8: {
+            name: "Strike Back",
+            desc: "Reach low health to reduce active skill cooldowns by 20%",
+            req: "",
+            benefits: ["Cooldown Reduction", "Affects Agent"],
+        }
+    },
+    tech: {
+        tal1: {
+            name: "Tactical Advance",
+            desc: "Complete a cover to cover move to increase weapon damage by 2% per meter covered for 10 seconds",
+            req: "Central Heating",
+            benefits: ["Weapon Damage", "Affects Agent"],
+        },
+        tal2: {
+            name: "Demolition Expert",
+            desc: "Kill a hostile with an explosion to increase explosion damage by 40% for 15 seconds",
+            req: "Central Heating",
+            benefits: ["Explosion Damage", "Affects Hostiles"],
+        },
+        tal3: {
+            name: "Police Up",
+            desc: "Kill a hostile with any skill for a 25% chance of auto-refilling all ammo types to capacity",
+            req: "SatComms",
+            benefits: ["Ammo Refill Chance", "Affects Agent"],
+        },
+        tal4: {
+            name: "Fear Tactics",
+            desc: "Applying Shock to any target triggers a 30% chance to apply the effect to all targets within 10m",
+            req: "SatComms",
+            benefits: ["Shock Chance", "Affects Hostiles"],
+        },
+        tal5: {
+            name: "Evasive Action",
+            desc: "During a cover to cover move incoming damage is reduced by 30%",
+            req: "Recalibration",
+            benefits: ["Damage Reduction", "Affects Agent"],
+        },
+        tal6: {
+            name: "Tech Support",
+            desc: "Kill a hostile while any skill is deployed to extend any active skills duration by 10%",
+            req: "Recalibration",
+            benefits: ["Skill Duration", "Affects Agent"],
+        },
+        tal7: {
+            name: "Wildfire",
+            desc: "Applying Burn to any target triggers a 30% chance to apply the effect to all targets within 10m",
+            req: "Recharge Center",
+            benefits: ["Burn Chance", "Affects Hostiles"],
+        },
+        tal8: {
+            name: "Death by Proxy",
+            desc: "Destroy a hostile's deployed skill to increase skill power by 20% for 30 seconds",
+            req: "Recharge Center",
+            benefits: ["Disruption", "Affects Hostiles"],
+        }
+    },
+    sec: {
+        tal1: {
+            name: "Steady Hands",
+            desc: "Enter any cover to reduce recoil by 25% for 10 seconds",
+            req: "Procurement Team",
+            benefits: ["Recoil Reduction", "Affects Agent"],
+        },
+        tal2: {
+            name: "On the Move",
+            desc: "Kill a hostile while moving to reduce incoming damage by 30% for 10 seconds",
+            req: "Procurement Team",
+            benefits: ["Damage Reduction", "Affects Agent"],
+        },
+        tal3: {
+            name: "Precision",
+            desc: "Headshot a hostile to pulse them for 10 seconds",
+            req: "Guard Posts",
+            benefits: ["Headshot Damage", "Affects Hostiles"],
+        },
+        tal4: {
+            name: "Chain Reaction",
+            desc: "Damage multiple hostiles with an explosion to apply 40% more damage from the blast",
+            req: "Guard Posts",
+            benefits: ["Explosion Damage", "Affects Hostiles"],
+        },
+        tal5: {
+            name: "Desperate Times",
+            desc: "Reach low health to increase blind fire accuracy by 20%",
+            req: "Shooting Range",
+            benefits: ["Blindfire Accuracy", "Affects Agent"],
+        },
+        tal6: {
+            name: "Stopping Power",
+            desc: "Suppress a hostile to increase headshot damage by 25% for 10 seconds",
+            req: "Shooting Range",
+            benefits: ["Headshot Damage", "Affects Hostiles"],
+        },
+        tal7: {
+            name: "Repo Reaper",
+            desc: "Kill a hostile with the sidearm to receive 1 magazine for your primary weapon",
+            req: "Canine Unit",
+            benefits: ["Ammo", "Affects Agent"],
+        },
+        tal8: {
+            name: "One is None",
+            desc: "Headshot a hostile to have a 50% chance of not consuming a the bullet",
+            req: "Canine Unit",
+            benefits: ["Bullet Consumption", "Affects Hostiles"],
+        }
+    },
+};
+
+
+
+var activeTalent = "tal1";
+var activeTree = "med";
+
+
+var choiceTalent = "talMed1";
+$(".talentBox").click(function() {
+
+    $(this).addClass("boxToggle");
+
+    if (choiceTalent === $(this).attr('id')) {
+
+    } else if (choiceTalent !== "") {
+        $("#" + choiceTalent).removeClass("boxToggle");
+        choiceTalent = "";
+        choiceTalent = $(this).attr('id');
+    }
+
+});
+
+function updateTalentIcons (tree) {
+	var y;
+	var tr;
+
+	if (tree === "med") {
+    y = 0;
+    ic = 0;
+    tr = "Med";
+  } else if (tree === "tech") {
+    y = 65;
+    ic = 100;
+    tr = "Tech";
+  } else if (tree === "sec") {
+    y = 130;
+    ic = 200;
+    tr = "Sec";
+  }
+
+  $("#tal"+tr+1).css("background-position", "0px -"+y+"px");
+  $("#tal"+tr+2).css("background-position", "-65px -"+y+"px");
+  $("#tal"+tr+3).css("background-position", "-130px -"+y+"px");
+  $("#tal"+tr+4).css("background-position", "-195px -"+y+"px");
+  $("#tal"+tr+5).css("background-position", "-260px -"+y+"px");
+  $("#tal"+tr+6).css("background-position", "-325px -"+y+"px");
+  $("#tal"+tr+7).css("background-position", "-390px -"+y+"px");
+  $("#tal"+tr+8).css("background-position", "-455px -"+y+"px");
+}
+
+updateTalentIcons('med');
+updateTalentIcons('tech');
+updateTalentIcons('sec');
+
+function updateTalentInfoIcon(tree) {
+	var y;
+	var tr;
+	var x;
+
+	if (tree === "med") {
+    y = 0;
+  } else if (tree === "tech") {
+    y = 100;
+  } else if (tree === "sec") {
+    y = 200;
+  }
+
+  x = parseFloat(activeTalent.slice(-1)-1) * 100;
+
+  $(".talInfoTalentIcon").css("background-position", "-"+x+"px -"+y+"px");
+}
+
+function talentCheck(tree) {
+    // Change talent name
+    $("#talInfoTalentName").text(talData[tree][activeTalent].name);
+    // Change talent description
+    $(".talInfoDesc").text(talData[tree][activeTalent].desc);
+    if (talData[tree][activeTalent].req === "") {
+        // Hide requirement if 'req' is empty
+        $('.talReq').css("display", "none");
+    } else if (talData[tree][activeTalent].req !== "") {
+        // Show requirement if present
+        $('.talReq').css("display", "block");
+        // Change req text
+        $(".talReqDetail").text(talData[tree][activeTalent].req);
+        // Change icon depending on tree
+        if (activeTree === "med") {
+            $('.talReqTree').text("Medical Tree Upgrade");
+            $('.talReqIcon').css("background-position", "0 0");
+        } else if (activeTree === "tech") {
+            $('.talReqTree').text("Tech Tree Upgrade");
+            $('.talReqIcon').css("background-position", "-50px 0");
+        } else {
+            $('.talReqTree').text("Security Tree Upgrade");
+            $('.talReqIcon').css("background-position", "-100px 0");
+        }
+    }
+    // Clear table
+    $('.benefitTable').html('<thead><tr><td><span style="margin-bottom: 0px;color:orange;">Benefits</span></td></tr></thead>');
+    // Loop through benefits array
+    var len = talData[tree][activeTalent].benefits.length;
+    for (i = 0; i < len; i++) {
+        $('.benefitTable thead:last').after('<tr><td>' + talData[tree][activeTalent].benefits[i] + '</td></tr>');
+    }
+    if (activeTree === "med") {
+        $('.talInfoBox').css("border-top", "2px #6ec449 solid");
+        $('.talInfoBox').css("border-bottom", "2px #6ec449 solid");
+    } else if (activeTree === "tech") {
+        $('.talInfoBox').css("border-top", "2px #ffca57 solid");
+        $('.talInfoBox').css("border-bottom", "2px #ffca57 solid");
+    } else if (activeTree === "sec") {
+        $('.talInfoBox').css("border-top", "2px #4084c5 solid");
+        $('.talInfoBox').css("border-bottom", "2px #4084c5 solid");
+    }
+}
+
+function talentClick(tree,number){
+	$("#tal"+tree+number).mousedown(function() {
+    activeTalent = 'tal'+number;
+    activeTree = tree.toLowerCase();
+    talentCheck(activeTree);
+    updateTalentInfoIcon(activeTree);
+	});
+}
+
+talentClick('Med',1);
+talentClick('Med',2);
+talentClick('Med',3);
+talentClick('Med',4);
+talentClick('Med',5);
+talentClick('Med',6);
+talentClick('Med',7);
+talentClick('Med',8);
+
+talentClick('Tech',1);
+talentClick('Tech',2);
+talentClick('Tech',3);
+talentClick('Tech',4);
+talentClick('Tech',5);
+talentClick('Tech',6);
+talentClick('Tech',7);
+talentClick('Tech',8);
+
+talentClick('Sec',1);
+talentClick('Sec',2);
+talentClick('Sec',3);
+talentClick('Sec',4);
+talentClick('Sec',5);
+talentClick('Sec',6);
+talentClick('Sec',7);
+talentClick('Sec',8);
