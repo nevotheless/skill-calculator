6d5d32743459a59c9e395afbbbf27f607b0e1e96
Tim Kopplow <tim@arctium.org>
2016-03-30 15:51:13 +0200

    forgot to change back the gulpfile task

--- gulpfile.js
+++ gulpfile.js
@@ -64,4 +64,4 @@ gulp.task('default', ['css', 'js', 'browser-sync'], function () {
     gulp.watch("app/*.html", ['bs-reload']);
 });
 
-gulp.task('build', ['css', 'js']);
+gulp.task('test', ['css', 'js']);
