0693cc4a97d75d3197c0b40ffcd3479cbfedf167
Tim Kopplow <tim@arctium.org>
2016-04-09 22:43:20 +0200

    some cleanup, icon handling and data initialization

--- src/app.js
+++ src/app.js
@@ -12,24 +12,36 @@ config(function($routeProvider, $locationProvider) {
 })
 
 app.controller('skillCtrl', function($scope, $http) {
+
     $http.get('data/skills.json')
         .then(function(res){
             $scope.medical = res.data.skills.medical;
             //$scope.context = res.data[medical][skill1];
             //console.log($scope.context);
 
-            $scope.changeContext = function(type, skills) {
-                $scope.skill = res.data.skills[type][skills];
-                $scope.mods = res.data.skills[type][skills].mods;
-                //console.log($scope.skills);
+            // initialize
+            $scope.skill = res.data.skills.medical[0];
+            $scope.mods  = res.data.skills.medical[0].mods;
+            $scope.mod   = $scope.mods[0];
+
+            $scope.getSkillIconClass = function(type, skill) {
+                return "icon-" + res.data.skills[type][skill].name.replace(/\s/g, '-');
+            }
+
+            // context changer
+            $scope.changeContext = function(type, skill) {
+                $scope.skill = res.data.skills[type][skill];
+                $scope.mods  = res.data.skills[type][skill].mods;
+                $scope.mod   = $scope.mods[0];
+                //console.log(res.data.skills.medical[0]);
                 //console.log($scope.context.mods);
             }
+
+            // subcontext changer
             $scope.changeSubContext = function(key) {
                 $scope.mod = $scope.mods[key];
-                console.log($scope.mod);
-                //console.log($scope.subContext);
+                //console.log($scope.mod);
             }
 
         });
-    $scope.something = "Hello";
 });
