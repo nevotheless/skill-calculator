7daed44bc0342a1c61e02b3961af4ee5f104805e
Tim Kopplow <tim@arctium.org>
2016-04-09 18:47:25 +0200

    re-implemented the whole skill and mod switching stuff with angular

--- src/app.js
+++ src/app.js
@@ -10,3 +10,26 @@ config(function($routeProvider, $locationProvider) {
     when("/perks", { templateUrl: "./partials/perks.partial.html" }).
     otherwise( { redirectTo: "/skills" })
 })
+
+app.controller('skillCtrl', function($scope, $http) {
+    $http.get('data/skills.json')
+        .then(function(res){
+            $scope.medical = res.data.skills.medical;
+            //$scope.context = res.data[medical][skill1];
+            //console.log($scope.context);
+
+            $scope.changeContext = function(type, skills) {
+                $scope.skill = res.data.skills[type][skills];
+                $scope.mods = res.data.skills[type][skills].mods;
+                //console.log($scope.skills);
+                //console.log($scope.context.mods);
+            }
+            $scope.changeSubContext = function(key) {
+                $scope.mod = $scope.mods[key];
+                console.log($scope.mod);
+                //console.log($scope.subContext);
+            }
+
+        });
+    $scope.something = "Hello";
+});
