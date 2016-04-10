a46c225b7edb0031269de717c7e8247a29e1e7c7
Tim Kopplow <tim@arctium.org>
2016-04-10 17:38:32 +0200

    added mod icon handling, mod handling in general, some signature skill adjustments and maybe much more

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.controller('skillCtrl', function($scope, $http) {
     $http.get('data/skills.json')
         .then(function(res){
             $scope.medical = res.data.skills.medical;
+            $scope.tec     = res.data.skills.tec;
+            $scope.sec     = res.data.skills.sec;
+
             //$scope.context = res.data[medical][skill1];
             //console.log($scope.context);
 
@@ -27,12 +30,28 @@ app.controller('skillCtrl', function($scope, $http) {
             $scope.getSkillIconClass = function(type, skill) {
                 return "icon-" + res.data.skills[type][skill].name.replace(/\s/g, '-');
             }
+            $scope.getModIconClass = function(skillName, identifier) {
+                return "icon-" + skillName.replace(/\s/g, '-') + '-' + identifier;
+            }
+            $scope.isSignature = function(skillName) {
+                if(skillName=="Recovery Link")
+                    return "hideSkillHead"
+                else if(skillName=="Tactical Link")
+                    return "hideSkillHead"
+                else if(skillName=="Survivor Link")
+                    return "hideSkillHead"
+            }
 
             // context changer
             $scope.changeContext = function(type, skill) {
                 $scope.skill = res.data.skills[type][skill];
                 $scope.mods  = res.data.skills[type][skill].mods;
                 $scope.mod   = $scope.mods[0];
+                
+                // change context skill icon and mod icons depending of the current skill context
+                $scope.skillIconClass = "icon-" + $scope.skill.name.replace(/\s/g, '-');
+                $scope.modIconClass = "icon-mod-" + $scope.mod.name.replace(/\s/g, '-');
+
                 //console.log(res.data.skills.medical[0]);
                 //console.log($scope.context.mods);
             }
@@ -40,6 +59,7 @@ app.controller('skillCtrl', function($scope, $http) {
             // subcontext changer
             $scope.changeSubContext = function(key) {
                 $scope.mod = $scope.mods[key];
+                //console.log($scope.skill.name);
                 //console.log($scope.mod);
             }
 
